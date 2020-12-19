8755fef2ff6ac32c4283c61c08c9ddf9b62bc1f5
Valentyna-Chudik <valentyna_chudik@ukr.net>
2020-12-19 13:24:13 +0200

    PropTypes added

--- src/components/GalleryView/GalleryView.js
+++ src/components/GalleryView/GalleryView.js
@@ -56,7 +56,7 @@ export default class GalleryView extends Component {
       })
 
       .catch(error => this.setState({ error }))
-      .finally(() => this.setState({ isLoading: false }), this.scrollPage());
+      .finally(() => this.setState({ isLoading: false }));
   };
 
   onLoadMore = () => {
@@ -114,3 +114,92 @@ export default class GalleryView extends Component {
     );
   }
 }
+
+// import { useState, useEffect } from 'react';
+// // import { toast } from 'react-toastify';
+
+// import imagesApi from '../../services/images-api';
+// import ImageGallery from '../ImageGallery/ImageGallery';
+// import Button from '../Button/Button';
+// import LoaderSpinner from '../Loader/Loader';
+// import Section from '../Section/Section';
+// import Modal from '../Modal/Modal';
+
+// export default function GalleryView() {
+//   const [images, setImages] = useState([]);
+//   const [currentPage, setCurrentPage] = useState(1);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [error, setError] = useState(null);
+//   const [showModal, setShowModal] = useState(false);
+//   const [largeImageURL, setLargeImageURL] = useState('');
+//   const [alt, setAlt] = useState('');
+//   const [query, setQuery] = useState('');
+
+//   useEffect(() => {
+//     if (!query) {
+//       return;
+//     }
+
+//     setIsLoading(true);
+
+//     imagesApi
+//       .fetchImages(query, currentPage)
+//       .then(images => {
+//         setImages(prevImages => [...prevImages, ...images]);
+//       })
+//       .catch(error => {
+//         setError({ error });
+//       })
+//       .finally(() => setIsLoading(false));
+//     imagesApi.fetchImages();
+//   }, [query, currentPage]);
+
+//   // const handleSubmit = newQuery => {
+//   //   if (newQuery !== query) {
+//   //     setImages([]);
+//   //     setQuery(newQuery);
+//   //     setCurrentPage(1);
+//   //   }
+//   // };
+
+//   const onLoadMore = () => {
+//     imagesApi.fetchImages();
+//     setCurrentPage(prevPage => prevPage + 1);
+//     scrollPage();
+//   };
+
+//   const scrollPage = () => {
+//     setTimeout(() => {
+//       window.scrollBy({
+//         top: document.documentElement.clientHeight - 160,
+//         behavior: 'smooth',
+//       });
+//     }, 500);
+//   };
+
+//   const toggleModal = () => {
+//     setShowModal(!showModal);
+//   };
+
+//   const onOpenModal = e => {
+//     setLargeImageURL(e.target.dataset.source);
+//     setAlt(e.target.alt);
+
+//     toggleModal();
+//   };
+
+//   const shouldRenderLoadMoreBtn = images.length > 0 && !isLoading;
+
+//   return (
+//     <div>
+//       <ImageGallery images={images} onClick={onOpenModal} />
+//       {showModal && (
+//         <Modal largeImgURL={largeImageURL} alt={alt} onClose={toggleModal} />
+//       )}
+//       {isLoading && <LoaderSpinner />}
+//       <Section>
+//         {shouldRenderLoadMoreBtn && <Button onLoadMore={onLoadMore} />}
+//       </Section>
+//     </div>
+//   );
+// }
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,38 +1,73 @@
-import React, { Component } from 'react';
-import { createPortal } from 'react-dom';
+// import React, { Component } from 'react';
+// import { createPortal } from 'react-dom';
 
+// import styles from './Modal.module.css';
+
+// const modalRoot = document.querySelector('#modal-root');
+
+// export default class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   handleBackdropClick = e => {
+//     if (e.currentTarget === e.target) {
+//       this.props.onClose();
+//     }
+//   };
+//   render() {
+//     return createPortal(
+//       <div className={styles.overlay} onClick={this.handleBackdropClick}>
+//         <div className={styles.modal}>
+//           <img src={this.props.largeImgURL} alt={this.props.alt} />
+//         </div>
+//       </div>,
+//       modalRoot,
+//     );
+//   }
+// }
+
+import React, { useEffect } from 'react';
+import { createPortal } from 'react-dom';
 import styles from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
+export default function Modal({ largeImgURL, alt, onClose }) {
+  useEffect(() => {
+    function handleKeyDown(e) {
+      if (e.code === 'Escape') {
+        onClose();
+      }
     }
-  };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
 
-  handleBackdropClick = e => {
+  const handleBackdropClick = e => {
     if (e.currentTarget === e.target) {
-      this.props.onClose();
+      onClose();
     }
   };
-  render() {
-    return createPortal(
-      <div className={styles.overlay} onClick={this.handleBackdropClick}>
-        <div className={styles.modal}>
-          <img src={this.props.largeImgURL} alt={this.props.alt} />
-        </div>
-      </div>,
-      modalRoot,
-    );
-  }
+
+  return createPortal(
+    <div className={styles.overlay} onClick={handleBackdropClick}>
+      <div className={styles.modal}>
+        <img src={largeImgURL} alt={alt} />
+      </div>
+    </div>,
+    modalRoot,
+  );
 }
